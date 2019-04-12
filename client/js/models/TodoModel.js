f148f263dfc4fa55e835ed5244184193354a6c93
MaxMEllon <maxmellon1994@gmail.com>
2019-04-12 14:12:10 +0900

    ちょっとリファクタ

--- client/js/models/TodoModel.js
+++ client/js/models/TodoModel.js
@@ -1,5 +1,5 @@
 class TodoModel {
-  constructor(id, name, done) {
+  constructor({ id, name, done }) {
     this.id = id
     this.name = name
     this.done = done
@@ -12,7 +12,7 @@ const TodoCollection = {
   async read() {
     const resp = await fetch('/todos').then((res) => res.json())
     this.todos = resp.todos.map((todo) => {
-      return new TodoModel(todo.name, todo.done)
+      return new TodoModel({ ...todo })
     })
     return this.todos
   }
